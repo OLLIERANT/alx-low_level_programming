2d6be908136abfce2acb78c9e71c43b7c260cca1
Olaoluwa Babalola <lalaboiydvibeplug@gmail.com>
2022-09-21 05:41:31 -0700

    All possible Combo

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of single digit numbers,
+ * in ascending order,
+ * must be seperated by comma,
+ * followed by a space
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+
+	}
+	putchar('\n');
+	return (0);
+}
