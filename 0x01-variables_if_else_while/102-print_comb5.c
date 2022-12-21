ebbcceed046d462504c3b8db6b3d4f5388c710d1
Olaoluwa Babalola <lalaboiydvibeplug@gmail.com>
2022-12-20 17:00:53 -0800

    newest doubledigit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,25 +9,25 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; i < 100; i++)
+	for (i = 0; i <= 98; i++)
 	{
-		for (j = 0; j < 100; j++)
-		{
-			if (i < j)
+		for (j = i + 1; j <= 99; j++)
 			{
-				putchar((i / 10) + 48);
-				putchar((i % 10) + 48);
+				putchar((i / 10) + '0');
+				putchar((i % 10) + '0');
 				putchar(' ');
-				putchar((j / 10) + 48);
-				putchar((j % 10) + 48);
+				putchar((j / 10) + '0');
+				putchar((j % 10) + '0');
+
 				if
-				  (i != 98 || j != 99);
+					(i == 98 && j == 99);
 				{
+					continue;
+
 					putchar(',');
 					putchar(' ');
 				}
 			}
-		}
 	}
 	putchar('\n');
 	return (0);
