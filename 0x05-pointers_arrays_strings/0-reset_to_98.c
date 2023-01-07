f6c69e685ff7dc7afb782bb6e2560df3da111f7b
Babalola Olaoluwa <lalaboiydvibeplug@gmail.com>
2023-01-06 20:35:27 -0800

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Takes a pointer as parameter
+ * and update the value to 98
+ *
+ * @n: An integer input pointer
+ *
+ * Return: nothing!
+ */
+void reset_to_98(int *n)
+{
+
+	*n = 98;
+}
