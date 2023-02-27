73662d178ff8e6578bbd7e7567a29d9bb741df87
Babalola Olaoluwa <lalaboiydvibeplug@gmail.com>
2023-02-26 16:20:00 -0800

    3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -28,7 +28,7 @@ list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new;
 	list_t *temp;
-	list_t length = _strlen(str);
+	int length = _strlen(str);
 
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
