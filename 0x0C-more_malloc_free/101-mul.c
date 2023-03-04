c89af5d95bcff9687455ee4bf3c8860f0658c49d
Babalola Olaoluwa <lalaboiydvibeplug@gmail.com>
2023-03-03 23:52:42 -0800

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -13,6 +13,7 @@ int main(int argc, char *argv[])
 {
 	unsigned long mul;
 	int i, j;
+
 	if (argc != 3)
 	{
 		printf("Error\n");
@@ -20,16 +21,16 @@ int main(int argc, char *argv[])
 	}
 	for (i = 1; i < argc; i++)
 	{
-		for (j = 0; argv[i][j] != "\0"; j++)
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
 			if (argv[i][j] > 57 || argv[i][j] < 48)
 			{
-				printf ("Error\n");
+				printf("Error\n");
 				exit(98);
 			}
 		}
 	}
-	mul = atol(argv[1]) *atol(argv[2]);
+	mul = atol(argv[1]) * atol(argv[2]);
 	printf("%lu\n", mul);
 	return (0);
 }
