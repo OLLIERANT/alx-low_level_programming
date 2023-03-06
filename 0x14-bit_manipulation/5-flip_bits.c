5ed4138aca11d2a2a5b986c2b10a23f2a8d76299
Babalola Olaoluwa <lalaboiydvibeplug@gmail.com>
2023-03-06 01:47:56 -0800

    5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -10,16 +10,13 @@
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned int xor = n ^ m;
-	int count = 0;
+	unsigned long int xor = n ^ m, bits = 0;
 
-	if (!n || !m)
-		return (0);
-	while (xor)
+	while (xor > 0)
 	{
-		if (xor & 1)
-			count++;
-		xor = xor >> 1;
+		bits += (xor & 1);
+		xor >>= 1;
 	}
-	return (count);
+
+	return (bits);
 }
